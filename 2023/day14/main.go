5cf8adde5070cd30482d8c25608337f2d6271bf5
Dewi Williams <dewi@dewiwilliams.co.uk>
2023-12-14 20:22:49 +0000

    Day 14 part 2

--- 2023/day14/main.go
+++ 2023/day14/main.go
@@ -3,21 +3,107 @@ package main
 import (
 	"adventofcode/util"
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 const emptyCell = 0
 const squareCell = 1
 const roundCell = 2
 
+const north = 0
+const east = 1
+const south = 2
+const west = 3
+
 func main() {
 	data, width := parseData("input.txt")
 
-	//printGrid(data, width)
 	fmt.Printf("Part 1: %d\n", part1(data, width))
+	fmt.Printf("Part 2: %d\n", part2(data, width))
+}
+func part2(grid []int, width int) int {
+	start, length := findCycle(grid, width)
+
+	cycleCount := 1000000000
+	cycleCount -= start
+	cycleCount %= length
+
+	cycleStartGrid := getGridAfterCycles(grid, width, start)
+	targetGrid := getGridAfterCycles(cycleStartGrid, width, cycleCount)
+
+	return getTotalLoad(targetGrid, width)
+}
+func getGridAfterCycles(grid []int, width, cycles int) []int {
+	result := make([]int, len(grid))
+	copy(result, grid)
+
+	for i := 0; i < cycles; i++ {
+		tiltNorth(result, width)
+		tiltWest(result, width)
+		tiltSouth(result, width)
+		tiltEast(result, width)
+	}
+
+	return result
+}
+func findCycle(gridSource []int, width int) (int, int) {
+	grid := make([]int, len(gridSource))
+	copy(grid, gridSource)
+
+	fingerprints := []string{}
+
+	for i := 0; ; i++ {
+		tiltNorth(grid, width)
+		tiltWest(grid, width)
+		tiltSouth(grid, width)
+		tiltEast(grid, width)
+
+		fingerprints = append(fingerprints, getFingerprint(grid))
+
+		start, length := analyseFingerprints(fingerprints)
+		if start != -1 && length != -1 {
+			return start + 1, length
+		}
+	}
+}
+func analyseFingerprints(fingerprints []string) (int, int) {
+	if len(fingerprints) < 2 {
+		return -1, -1
+	}
+
+	for i := 1; i < len(fingerprints); i++ {
+		for j := 0; j < i; j++ {
+			if fingerprints[i] == fingerprints[j] {
+				return j, i - j
+			}
+		}
+	}
+
+	return -1, -1
+}
+func getFingerprint(grid []int) string {
+	parts := []string{}
+
+	for i, v := range grid {
+		if v == roundCell {
+			parts = append(parts, strconv.Itoa(i))
+		}
+	}
+
+	return strings.Join(parts, "_")
+}
+func areGridsEqual(grid1, grid2 []int) bool {
+	for i := range grid1 {
+		if grid1[i] != grid2[i] {
+			return false
+		}
+	}
+
+	return true
 }
 func part1(grid []int, width int) int {
 	tiltNorth(grid, width)
-	//printGrid(grid, width)
 
 	return getTotalLoad(grid, width)
 }
@@ -39,6 +125,66 @@ func getTotalLoad(grid []int, width int) int {
 	}
 
 	return result
+}
+func tiltEast(grid []int, width int) {
+	height := len(grid) / width
+
+	for y := 0; y < height; y++ {
+		for x := width - 1; x >= 0; x-- {
+			cell := x + y*width
+			if grid[cell] != roundCell {
+				continue
+			}
+
+			targetCell := getNextEmptyCell(grid, cell, 1, width-x-1)
+			if cell == targetCell {
+				continue
+			}
+
+			grid[targetCell] = roundCell
+			grid[cell] = emptyCell
+		}
+	}
+}
+func tiltWest(grid []int, width int) {
+	height := len(grid) / width
+
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			cell := x + y*width
+			if grid[cell] != roundCell {
+				continue
+			}
+
+			targetCell := getNextEmptyCell(grid, cell, -1, x)
+			if cell == targetCell {
+				continue
+			}
+
+			grid[targetCell] = roundCell
+			grid[cell] = emptyCell
+		}
+	}
+}
+func tiltSouth(grid []int, width int) {
+	height := len(grid) / width
+
+	for y := height - 1; y >= 0; y-- {
+		for x := 0; x < width; x++ {
+			cell := x + y*width
+			if grid[cell] != roundCell {
+				continue
+			}
+
+			targetCell := getNextEmptyCell(grid, cell, width, height-y-1)
+			if cell == targetCell {
+				continue
+			}
+
+			grid[targetCell] = roundCell
+			grid[cell] = emptyCell
+		}
+	}
 }
 func tiltNorth(grid []int, width int) {
 	height := len(grid) / width
